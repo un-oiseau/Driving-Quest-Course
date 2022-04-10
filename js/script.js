e29b09c2dba8256e8d26083bc1872bfab9055250
Narine Harutyunyan <narharut208@gmail.com>
2022-04-11 00:47:28 +0400

    First commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(function(){
+    $('.register-purchase .row .col-md-6:nth-child(2)').load('https://course.drivingquest.com/api/register/purchase/parent-taught-course?source=drivingquest-express');
+  });
+
+  
