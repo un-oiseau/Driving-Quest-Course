30aa84dbed491e31c513c30aa7d34f8b07743065
Narine Harutyunyan <narharut208@gmail.com>
2022-04-14 01:22:06 +0400

    Optimized the script code for prohibitting button functionality

--- js/script.js
+++ js/script.js
@@ -1,42 +1,26 @@
 $(function(){
-    // $('.register-purchase .row .col-md-6:nth-child(2)').load('https://course.drivingquest.com/api/register/purchase/parent-taught-course?source=drivingquest-express');
-    $( ".btn-prohibit" ).click(function() {
-      var $el1 = $(".btn-no"),
-      x =500,
-      originalColor = $el1.css("background");
-      $el1.css("background", "rgba(159, 210, 219, 0.623)");
-      setTimeout(function(){
-        $el1.css("background", originalColor);
-      }, x);
-    });
+let prohibit = $(".btn-prohibit");
+let $el1 = $(".btn-no");
+let $el2 = $(".btn-email");
+let $el3 = $(".btn-fedex");
+originalColor1 = $el1.css("background");
+originalColor2 = $el2.css("background");
+originalColor3 = $el3.css("background");
+x =500;
+
+prohibit.on("click", function(){
+  let timespan =500;
+  $(".btn-no").css({"background": "rgba(159, 210, 219, 0.623)"});
+  $(".btn-email").css({"background": "rgba(159, 210, 219, 0.623)"});
+  $(".btn-fedex").css({"background": "rgba(159, 210, 219, 0.623)"});
+
+  setTimeout(function(){
+    $el1.css("background", originalColor1);
+    $el2.css("background", originalColor2);
+    $el3.css("background", originalColor3);
+    }, timespan);
   });
-
-
-  $(function(){
-    $( ".btn-prohibit" ).click(function() {
-      var $el2 = $(".btn-email"),
-      x =500,
-      originalColor2 = $el2.css("background");
-      $el2.css("background", "rgba(159, 210, 219, 0.623)");
-      setTimeout(function(){
-        $el2.css("background", originalColor2);
-      }, x);
-    });
-  });
-
-  $(function(){
-    $( ".btn-prohibit" ).click(function() {
-      var $el3 = $(".btn-fedex"),
-      x =500,
-      originalColor3 = $el3.css("background");
-      $el3.css("background", "rgba(159, 210, 219, 0.623)");
-      setTimeout(function(){
-        $el3.css("background", originalColor3);
-      }, x);
-    });
-
-    
-  });
+});
 
 
 
